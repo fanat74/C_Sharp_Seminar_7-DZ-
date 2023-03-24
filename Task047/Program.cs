f86cc2ac2844dd24ca20c6f582f967a77780511e
Ildar <akhmilg@gmail.com>
2023-03-23 21:07:03 -0700

    Added Task047

---
+++ Task047/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5    7  -2   -0,2
+// 1   -3,3   8   -9,9
+// 8    7,8  -7,1    9
+
+double[,] CreateMatrix(int raws, int columns, double min, double max) //код работает но почему-то тип массива подчеркивает ошибкой
+
+{
+    double[,] matrix = new double[raws, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(min+ rnd.NextDouble()*(max-min), 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i,j], 6} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
+double[,] array2D = CreateMatrix(3, 4, -10, 10);
+PrintMatrix(array2D);
